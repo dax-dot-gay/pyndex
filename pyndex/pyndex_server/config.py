b949b4fc4f7d1e25373896994415ed98ab9a0d07
Dax Harris <matteovh@gmail.com>
2024-04-25 11:32:31 -0400

    working configurable proxy

--- pyndex/pyndex_server/config.py
+++ pyndex/pyndex_server/config.py
@@ -1,5 +1,6 @@
 import tomllib
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, Field
 
 
 class StorageConfig(BaseModel):
@@ -10,11 +11,31 @@ class ApiConfig(BaseModel):
     path_base: str
 
 
+class ProxyItemConfig(BaseModel):
+    name: str
+    priority: int
+    url: str
+    username: Optional[str] = None
+    password: Optional[str] = None
+
+
+class FeatureConfig(BaseModel):
+    proxy: bool = True
+
+
 class Config(BaseModel):
     storage: StorageConfig
     api: ApiConfig
+    proxy: dict[str, ProxyItemConfig] = {}
+    features: FeatureConfig = Field(default_factory=FeatureConfig)
 
     @classmethod
     def load(cls) -> "Config":
         with open("./config.toml", "rb") as config:
             return cls(**tomllib.load(config))
+
+    @property
+    def proxies(self) -> list[ProxyItemConfig]:
+        if self.features.proxy:
+            return sorted(list(self.proxy.values()), key=lambda x: x.priority)
+        return []
--- pyndex/pyndex_server/routes/pypi.py
+++ pyndex/pyndex_server/routes/pypi.py
@@ -1,5 +1,6 @@
 import os
 from typing import Annotated
+from httpx import AsyncClient
 from litestar import Controller, post, get, Request
 from ..models import FileMetadata, Package, PackageList, PackageListItem, PackageDetail
 from ..context import Context
@@ -35,6 +36,16 @@ class PypiController(Controller):
         self, context: Context, project_name: str, request: Request
     ) -> PackageDetail:
         if not os.path.exists(context.root.joinpath("index", project_name)):
+            if len(context.config.proxies) > 0:
+                for proxy in context.config.proxies:
+                    async with AsyncClient(follow_redirects=True) as client:
+                        result = await client.get(
+                            proxy.url.rstrip("/") + "/" + project_name,
+                            headers={"Accept": "application/vnd.pypi.simple.v1+json"},
+                        )
+                        if result.is_success:
+                            return result.json()
+
             raise NotFoundException(f"Unknown project {project_name}.")
 
         base_url = str(request.base_url).rstrip("/")
